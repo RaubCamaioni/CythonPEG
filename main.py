408d5089954357c804b5a1c2d2031b7308e7129d
Raub Camaioni <raub.j.camioni.civ@army.mil>
2023-07-25 01:14:28 -0400

    almost done

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from pyparsing import *
 from collections.abc import Iterable
 from functools import partial
 
+def parentheses_suppress(content):
+    return Suppress("(") + Optional(content) + Suppress(")")
+
+def bracket_suppress(content):
+    return Suppress("[") + Optional(content) + Suppress("]")
+
+def curl_suppress(content):
+    return Suppress("{") + Optional(content) + Suppress("}")
+
 # literals
 CLASS = Literal("class")
 DEF = Literal("def")
@@ -9,10 +18,25 @@ CPDEF = Literal("cpdef")
 CDEF = Literal("cdef")
 STRUCT = Literal("struct")
 
-def lollipop(candy):
-    return Suppress("(") + Optional(candy) + Suppress(")")
+# objects
+INTEGER = Group(Word("+-" + nums) + ~FollowedBy("."))
+FLOAT = Combine(Word("+-" + nums) + "." + Word(nums))
+TRUE = Literal("True")
+FALSE = Literal("False")
+NONE = Literal("None")
+STRING = QuotedString(quoteChar="'") | QuotedString(quoteChar='"')
+PRIMATIVE = (FLOAT | INTEGER | TRUE | FALSE | NONE | STRING)
+OBJECT = Forward()
+LIST = Group(bracket_suppress(delimitedList(OBJECT)))
+TUPLE = Group(parentheses_suppress(delimited_list(OBJECT)))
+DICT = Group(curl_suppress(delimitedList(Group(OBJECT + Suppress(":") + OBJECT))))
+SET = Group(curl_suppress(delimited_list(OBJECT)))
+NONPRIMATIVE = (LIST | TUPLE | DICT | SET)
+OBJECT << (PRIMATIVE | NONPRIMATIVE)
 
-DOptional = partial(Optional, default=[])
+# variable and type names
+VARIABLE = Word(alphanums+'_')
+TYPE = (Word(alphanums+'_'+'*'+'.') | Group(parentheses_suppress(delimited_list(Word(alphanums+'_'+'*'+'.')))))
 
 recursive_class_definition = Forward()
 recursive_def_definition = Forward()
@@ -21,55 +45,48 @@ recursive_cython_class_definition = Forward()
 recursive_cython_struct_definition = Forward()
 
 # python type hint and argument
-identifier = Word(alphanums+'_'+'*')
-type_hint = Suppress(":") + identifier
-default_argument = Suppress("=") + identifier
-argument = Group(identifier + Optional(type_hint) + Optional(default_argument))
+default_argument = Suppress("=") + OBJECT
+argument = Group(VARIABLE + Optional(Suppress(":") + TYPE) + Optional(default_argument)) # VARIABLE:TYPE=DEFAULT
 arguments = delimitedList(argument)
-return_definition = (Suppress("->") + identifier)
+return_definition = (Suppress("->") + VARIABLE)
 
 # cython type hint and argument (simpler because cython is more structured than python)
-colon_list = nested_expr("[", "]", content=delimitedList(Literal(":"), delim=","))
-cython_argument = Group(Group(identifier + Optional(colon_list)) + identifier + Optional(default_argument))
+cython_type_hint_list = bracket_suppress(delimitedList(Literal(":") | Group(VARIABLE+Optional(default_argument))))
+cython_type_hint  = Group(TYPE + Optional(cython_type_hint_list))
+cython_argument = Group(cython_type_hint + VARIABLE + Optional(default_argument))
 cython_arguments = delimitedList(cython_argument)
 
 # docstring definition
 docstring = QuotedString('"""', multiline=True, escQuote='""""')
 
 # python class definition
-class_arguments = Group(lollipop(arguments))
-class_decleration = (CLASS + identifier + DOptional(class_arguments) + Optional(return_definition) + Suppress(":"))
+class_parent = Word(alphanums+'_'+'*'+'.')
+class_arguments = Group(parentheses_suppress(delimitedList(class_parent)))
+class_decleration = Group(CLASS + VARIABLE + Optional(class_arguments, default=[]) + Suppress(":"))
 class_body = IndentedBlock(recursive_class_definition, recursive=True)
-class_definition = Group(class_decleration + DOptional(docstring) + class_body)
+class_definition = class_decleration + Optional(docstring, default="") + class_body
 
 # python function definitions
-function_arguments = Group(lollipop(arguments))
-function_decleration = Group(DEF + identifier + function_arguments + DOptional(return_definition) + Suppress(":"))
+function_arguments = Group(parentheses_suppress(arguments))
+function_decleration = Group(DEF + VARIABLE + function_arguments + Optional(return_definition) + Suppress(":"))
 function_body = IndentedBlock(recursive_def_definition, recursive=True)
-function_definition = Group(function_decleration + DOptional(docstring) + function_body)
+function_definition = function_decleration + Optional(docstring, default="") + function_body
 
 # cython function definition 
-cython_function_arguments = Group(lollipop(cython_arguments))
-cython_function_decleration = Group(Group((CDEF | CPDEF)) + identifier + cython_function_arguments + DOptional(identifier) + Suppress(":"))
+cython_function_arguments = Group(parentheses_suppress(cython_arguments))
+cython_function_decleration = Group(Group((CDEF | CPDEF) + Optional(cython_type_hint + ~cython_function_arguments)) + Group(VARIABLE + cython_function_arguments) + Optional(VARIABLE) + Suppress(":")) 
 cython_function_body = IndentedBlock(recursive_cython_def_definition, recursive=True)
-cython_function_definition = Group(cython_function_decleration + DOptional(docstring) + cython_function_body)
-
-# split version
-cython_function_arguments = Group(lollipop(cython_arguments))
-# optional position very important, must be on second identifier NOT first
-cython_function_decleration = Group(Group((CDEF | CPDEF) + identifier + Optional(identifier)) + cython_function_arguments + DOptional(identifier) + Suppress(":")) 
-cython_function_body = IndentedBlock(recursive_cython_def_definition, recursive=True)
-cython_function_definition = Group(cython_function_decleration + DOptional(docstring) + cython_function_body)
+cython_function_definition = cython_function_decleration + Optional(docstring, default="") + cython_function_body
 
 # cython class definition
-cython_class_decleration = Group(Group(CDEF + CLASS) + identifier +  Suppress(":"))
+cython_class_decleration = Group(Group(CDEF + CLASS) + VARIABLE +  Suppress(":"))
 cython_class_body = IndentedBlock(recursive_cython_class_definition, recursive=True)
-cython_class_definition = Group(cython_class_decleration + Optional(docstring) + cython_class_body)
+cython_class_definition = cython_class_decleration + Optional(docstring, default="") + cython_class_body
 
 # cython struct definition
-cython_struct_decleration = Group(Group(CDEF + STRUCT) + identifier + Suppress(":"))
+cython_struct_decleration = Group(Group(CDEF + STRUCT) + VARIABLE + Suppress(":"))
 cython_struct_body = IndentedBlock(recursive_cython_struct_definition, recursive=True)
-cython_struct_definition = Group(cython_struct_decleration + Optional(docstring) + cython_struct_body)
+cython_struct_definition = cython_struct_decleration + Optional(docstring, default="") + cython_struct_body
 
 recursive_class_definition         << (class_definition | function_definition | cython_function_definition | cython_class_definition | cython_struct_definition | restOfLine)
 recursive_def_definition           << (class_definition | function_definition | cython_function_definition | cython_class_definition | cython_struct_definition | restOfLine)
@@ -79,14 +96,13 @@ recursive_cython_struct_definition << (class_definition | function_definition | 
 
 python_script_parser = class_definition | function_definition | cython_function_definition | cython_class_definition | cython_struct_definition
 
-with open("cython_test.py", mode="r") as f:
-    input_code = f.read()
-
 # targeted testing
 input_code = """
+cpdef tuple warped_image_from_rays(float64[:, :, fut=[1, 2, 3]] image, float64 rays, float64 sample_freq=-1.0):
+    pass
 """
 
-with open("cython_test.py", mode="r") as f:
+with open("local/test.py", mode="r") as f:
     input_code = f.read()
 
 # Parse the input code using the defined grammar
@@ -95,9 +111,10 @@ parseTree = python_script_parser.scan_string(input_code)
 for branch in parseTree:
     result, startline, tokens = branch
 
-    print(result[0][0])
+    decleration, docs, body = result
 
-    # decleration = result[0][0]
-    # print(decleration)
-
-# parseTree.pprint()
+    print(decleration)
+    for b in body:
+        if len(b) > 0:
+            if b[0] == "def":
+                print("\t", b)
