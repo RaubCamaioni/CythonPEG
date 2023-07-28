8182dbb8ee235fb91945aa37cb99c2bd3ce437e3
Raub Camaioni <raub.j.camioni.civ@army.mil>
2023-07-28 19:40:19 -0400

    working reqork

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def extend_empty(tokens, n):
         tokens.extend([""]*n)
     return tokens
 
+def EmptyDefault(input, n=1):
+    return Optional(input).addParseAction(partial(extend_empty, n=n))
+
 # literal definitions
 CLASS = Literal("class")
 STRUCT = Literal("struct")
@@ -33,10 +36,11 @@ PLUS = Literal('+')
 MINUS = Literal('-')
 MULT = Literal('*')
 DIV = Literal('/')
+RETURN = Literal("->")
 
 
 # object definitions
-VARIABLE = Word(alphanums+"_")
+VARIABLE = Word("_"+alphanums+"_"+".")
 INTEGER = Word("+-" + nums) + ~FollowedBy(".")
 FLOAT = Combine(Word("+-" + nums) + "." + Word(nums))
 TRUE = Literal("True")
@@ -54,80 +58,38 @@ CLASS_CONSTRUCTOR = Group(VARIABLE+delimited_list(parentheses_suppress(OBJECT)))
 NONPRIMATIVE = (LIST | TUPLE | DICT | SET | ENUM | CLASS_CONSTRUCTOR)
 OBJECT << (NONPRIMATIVE | PRIMATIVE)
 
+# EXPRESSION definition
+EXPRESSION = Forward()
+ATOM = OBJECT | Group(Literal('(') + EXPRESSION + Literal(')'))
+EXPRESSION << infixNotation(ATOM, [(MULT | DIV, 2, opAssoc.LEFT), (PLUS | MINUS, 2, opAssoc.LEFT),])
+
+# default_definition (part of type definition)
+default_definition = (EQUALS + EXPRESSION)("default")
+
 # type definitions
 type_forward = Forward()
 type_bracket = bracket_suppress(delimited_list(type_forward))
-type_definition  = (OneOrMore(VARIABLE + Optional(DOT)) + Optional(Group(type_bracket)))("type")
+# type_definition = Group((OneOrMore(VARIABLE + Optional(DOT))) + EmptyDefault(Group(type_bracket)) + EmptyDefault(default_definition+ ~Literal(')')))("type")
+type_definition = Group(VARIABLE + EmptyDefault(Group(type_bracket)) + EmptyDefault(default_definition+ ~Literal(')')))("type")
 type_forward << type_definition
 
+# return definitions
+python_return_definition = Suppress(RETURN) + type_definition
+
 # argument definition
-argument_default = (EQUALS + OBJECT)("default")
-argument_definition = (VARIABLE("name") + Optional(COLON + type_definition) + Optional(argument_default))("argument")
+python_argument_definition = Group(VARIABLE("name") + EmptyDefault(COLON + type_definition, 1) + EmptyDefault(default_definition))("argument")
+cython_argument_definition = Group(type_definition + VARIABLE("name") + EmptyDefault(default_definition))("arguments")
 
 # arguments definition
-arguments_definition = parentheses_suppress(delimited_list(argument_definition))("arguments")
+python_arguments_definition = parentheses_suppress(delimited_list(python_argument_definition))("arguments")
+cython_arguments_definition = parentheses_suppress(delimited_list(cython_argument_definition))("arguments")
 
-import sys
-sys.exit()
-
-# def argument_definition():
-#     identifier = Word(alphanums)
-#     argument_forward = Forward()
-#     bracketed_arguments = Group(bracket_suppress(delimited_list(argument_forward)))
-#     argument = Group(OneOrMore(identifier+Optional(DOT)) + Optional(bracketed_arguments))
-#     argument_forward << bracketed_arguments
-#     return argument
-
-# objects
-INTEGER = Word("+-" + nums) + ~FollowedBy(".")
-FLOAT = Combine(Word("+-" + nums) + "." + Word(nums))
-TRUE = Literal("True")
-FALSE = Literal("False")
-NONE = Literal("None")
-STRING = QuotedString(quoteChar="'") | QuotedString(quoteChar='"')
-ENUM = Word(alphanums + '.')
-PRIMATIVE = (FLOAT | INTEGER | TRUE | FALSE | NONE | STRING)
-OBJECT = Forward()
-LIST = Group(bracket_suppress(delimitedList(OBJECT)))
-TUPLE = Group(parentheses_suppress(delimited_list(OBJECT)))
-DICT = Group(curl_suppress(delimitedList(Group(OBJECT + Suppress(":") + OBJECT))))
-SET = Group(curl_suppress(delimited_list(OBJECT)))
-NONPRIMATIVE = (LIST | TUPLE | DICT | SET | ENUM)
-OBJECT << (NONPRIMATIVE | PRIMATIVE)
-
-
-# Define grammar for the entire expression
-expression = Forward()
-atom = OBJECT | Group(Literal('(') + expression + Literal(')'))
-expression << infixNotation(atom, [(MULT | DIV, 2, opAssoc.LEFT), (PLUS | MINUS, 2, opAssoc.LEFT),])
-
-# variable and type names
-VARIABLE = Word(alphanums+'_')
-TWORD = Word(alphanums+'_'+'*'+'.')
-TYPE = (TWORD) | Group(parentheses_suppress(delimited_list(TWORD)))
-
-OPTIONALBRACKET = Group(Optional(bracket_suppress(delimited_list(TWORD))))
-
+# recursive definitions 
 recursive_class_definition = Forward()
 recursive_def_definition = Forward()
 recursive_cython_def_definition = Forward()
 recursive_cython_class_definition = Forward()
 recursive_cython_struct_definition = Forward()
-
-# python type hint and arguments
-empty_default_return = ParseResults()
-empty_default_return.extend(["", ""])
-
-default_argument = Suppress("=") + expression
-argument = Group(VARIABLE + Optional(Suppress(":") + Group(TYPE + OPTIONALBRACKET), default=["", ""]) + Optional(default_argument, default="")) # VARIABLE:TYPE=DEFAULT
-arguments = delimitedList(argument)
-return_definition = Group(Suppress("->") + TYPE + OPTIONALBRACKET)
-
-# cython type hint and arguments
-cython_type_hint_list = Group(bracket_suppress(delimitedList(Literal(":") | Group(VARIABLE+Optional(default_argument, default="")))))
-cython_type_hint  = (TYPE + Optional(cython_type_hint_list, default=""))
-cython_argument = Group(cython_type_hint + VARIABLE + Optional(default_argument, default=""))
-cython_arguments = delimitedList(cython_argument)
 
 # docstring definition
 docstring = QuotedString('"""', multiline=True, escQuote='""""')
@@ -135,36 +97,34 @@ docstring = QuotedString('"""', multiline=True, escQuote='""""')
 # python class definition
 class_parent = Word(alphanums+'_'+'*'+'.')
 class_arguments = parentheses_suppress(class_parent)
-class_decleration = Group(CLASS + VARIABLE + Optional(class_arguments, default="") + Suppress(":"))
+class_decleration = Group(Suppress(CLASS) + VARIABLE + EmptyDefault(class_arguments) + Suppress(":"))("class_decleration")
 class_body = IndentedBlock(recursive_class_definition, recursive=True)
-class_definition = class_decleration + Optional(docstring, default="") + class_body
+class_definition = (class_decleration + Optional(docstring, default="") + class_body)("class")
 
 # python function definitions
-function_arguments = Group(parentheses_suppress(arguments))
-function_decleration = Group(DEF + VARIABLE + function_arguments + Optional(return_definition, default=["", []]) + Suppress(":"))
+function_decleration = Group(Suppress(DEF) + VARIABLE + Group(python_arguments_definition) + Optional(python_return_definition, "") + Suppress(":"))("def_decleration")
 function_body = IndentedBlock(recursive_def_definition, recursive=True)
-function_definition = function_decleration + Optional(docstring, default="") + function_body
+function_definition = (function_decleration + Optional(docstring, default="") + function_body)("def")
 
 # cython function definition 
-cython_function_arguments = Group(parentheses_suppress(cython_arguments))
-cython_function_decleration = Group(Group((DEF | CDEF | CPDEF) + Optional(cython_type_hint + ~cython_function_arguments, default="")) + VARIABLE + cython_function_arguments + Optional(VARIABLE, default="") + Suppress(":")) 
+cython_function_decleration = Group(Suppress((DEF | CDEF | CPDEF) + Optional(type_definition + ~cython_arguments_definition, default="")) + VARIABLE + cython_arguments_definition + Optional(VARIABLE, default="") + Suppress(":")) 
 cython_function_body = IndentedBlock(recursive_cython_def_definition, recursive=True)
-cython_function_definition = cython_function_decleration + Optional(docstring, default="") + cython_function_body
+cython_function_definition = (cython_function_decleration + Optional(docstring, default="") + cython_function_body)("cdef")
 
 # cython class definition
-cython_class_decleration = Group(Group(CDEF + CLASS) + VARIABLE +  Suppress(":"))
+cython_class_decleration = Group(Suppress(CDEF + CLASS) + VARIABLE +  Suppress(":"))
 cython_class_body = IndentedBlock(recursive_cython_class_definition, recursive=True)
-cython_class_definition = cython_class_decleration + Optional(docstring, default="") + cython_class_body
+cython_class_definition = (cython_class_decleration + Optional(docstring, default="") + cython_class_body)("cclass")
 
 # cython struct definition
-cython_struct_decleration = Group(Group(CDEF + STRUCT) + VARIABLE + Suppress(":"))
+cython_struct_decleration = Group(Suppress(CDEF + STRUCT) + VARIABLE + Suppress(":"))
 cython_struct_body = IndentedBlock(recursive_cython_struct_definition, recursive=True)
-cython_struct_definition = cython_struct_decleration + Optional(docstring, default="") + cython_struct_body
+cython_struct_definition = (cython_struct_decleration + Optional(docstring, default="") + cython_struct_body)("cstruct")
 
 # dataclass definition
-dataclass_decleration = Group(Suppress("@") + DATACLASS + CLASS + VARIABLE + Suppress(":"))
+dataclass_decleration = (Suppress(Literal("@") + DATACLASS + CLASS) + VARIABLE + Suppress(":"))
 dataclass_body = IndentedBlock(rest_of_line, recursive=True)
-dataclass_definition = dataclass_decleration + Optional(docstring, default="") + dataclass_body
+dataclass_definition = (dataclass_decleration + Optional(docstring, default="") + dataclass_body)("dataclass")
 
 # recursive definitions
 definitions = (class_definition | function_definition | cython_function_definition | cython_class_definition | cython_struct_definition | restOfLine)
@@ -176,42 +136,78 @@ recursive_cython_struct_definition << definitions
 
 cython_parser = class_definition | function_definition | cython_function_definition | cython_class_definition | cython_struct_definition | dataclass_definition
 
+def expression2str(expression):
+    
+    expression_string = ""
+    
+    if isinstance(expression, ParseResults):
+             
+        if expression.getName() == 'list':
+            expression_string += "[" + ', '.join(expression2str(e) for e in expression) + "]"
+
+        elif expression.getName() == 'set':
+            expression_string += "{" + ', '.join(expression2str(e) for e in expression) + "}"
+            
+        elif expression.getName() == 'tuple':
+            expression_string += "(" + ', '.join(expression2str(e) for e in expression) + ")"
+            
+        elif expression.getName() == 'dict':
+            expression_string += "{" + ', '.join(f"{expression2str(k)} : {expression2str(v)}" for k, v in expression) + "}"
+        
+        elif isinstance(expression, ParseResults):
+            for e in expression:
+                expression_string += expression2str(e)
+            
+    if isinstance(expression, str):
+        return expression
+    
+    return expression_string        
+        
+def arg2str(arg):
+    
+    arg_name, arg_type, arg_default = arg
+
+    if arg_type:
+        type_name, type_bracket, type_default = arg_type
+        bracket_str = "["+", ".join(arg2str(arg) for arg in type_bracket)+"]" if type_bracket else ""
+        type_default_str = f'={expression2str(type_default)}' if type_default else ''
+        type_name = type_name + bracket_str + type_default_str
+    else:
+        type_name = ""
+
+    type_str = f': {type_name}' if type_name else ''
+    arg_default_str = f'={expression2str(arg_default)}' if arg_default else ''
+    
+    return f'{arg_name}{type_str}{arg_default_str}'
+    
 def args2str(args, newlines=False):
     """convert argument format List[(name, type, default)] into a string representation."""
-
-    def format_arg(n, t, d):
-
-        t = t[0]
-        if isinstance(t, ParseResults):
-            t = f"{t[0]}[{','.join([t for t in t[1:]])}]"
-
-        type_str = f': {t}' if t else ''
-        default_str = f' = {d}' if d else ''
-        return f'{n}{type_str}{default_str}'
-
     joiner = ',\n    ' if newlines else ', '
-    return joiner.join(format_arg(n, t, d) for n, t, d in args)
+    return joiner.join(arg2str(arg) for arg in args)
 
 def def2str(name, args, ret, docs, indent=0):
-
-    if len(ret[0]) == 0:
+    
+    
+    if not ret:
         return_str = ""
+    else:
+        return_str = arg2str(ret)
+    
+    # elif isinstance(ret[0], str):
+    #     return_str = ret[0]
 
-    elif isinstance(ret[0], str):
-        return_str = ret[0]
+    #     if isinstance(ret[1], ParseResults) and len(ret[1]):
+    #         return_str = return_str + f"[{', '.join([r for r in ret[1]])}]"
 
-        if isinstance(ret[1], ParseResults) and len(ret[1]):
-            return_str = return_str + f"[{', '.join([r for r in ret[1]])}]"
-
-    # syntax is not valid for the following
-    elif isinstance(ret[0], ParseResults):
-        return_str = f"{ret[0]}[{', '.join([t for t in ret[1:]])}]"
+    # # syntax is not valid for the following
+    # elif isinstance(ret[0], ParseResults):
+    #     return_str = f"{ret[0]}[{', '.join([t for t in ret[1:]])}]"
 
     return_str = f" -> {return_str}" if return_str else ''
     doc_str = f'\n    \"""{docs}\"""' if docs else ''
+    
     arg_str = args2str(args)
-
-    if len(arg_str) > 150:
+    if len(arg_str) > 100:
         arg_str = args2str(args, newlines=True)
 
     return textwrap.indent(f"def {name}({arg_str}){return_str}:{doc_str}\n    ...", "    "*indent)
@@ -285,44 +281,44 @@ def parse_tree_to_stub_file(parseTree):
 
     for branch in parseTree:
         result, _, _ = branch
-
-        decleration, docs, body = result
-        state = decleration[0]
         
-        if state == "def":
-            _, name, args, ret = decleration
+        definitionName = result.getName()
+        
+        if definitionName == "def":
+            decleration, docs, body = result
+            name, args, ret = decleration
             stub_file += def2str(name, args, ret, docs) + '\n'*2
 
-        elif len(state) > 0 and state[0] == "cdef":
-
-            if decleration[0][1] in ["class", "struct"]:
-
-                stub_file += class2str(decleration[1], "", docs) + "\n"
-                
-                if decleration[0][1] == "struct":
-                    for b in body:
-                        stub_file += "    " + b + '\n'
-                    stub_file += "\n"
-                
-                elif decleration[0][1] == "class":
-                    for i, b in enumerate(body):
-                        if len(b) > 0 and b[0][0] in ["def", "cdef"]:
-                            docs = body[i+1]
-                            ret, name, args, gil = b
-                            stub_file += '\n' + cdef2str(ret, name, args, gil, docs, indent=1) + '\n'
-                    stub_file += '\n'
-                        
-                continue
-            else:
-                ret, name, args, gil = decleration
-                stub_file += cdef2str(ret, name, args, gil, docs) + '\n'*2
-
-        elif len(state) > 0 and state[0] == "cpdef":
+        elif definitionName == "cdef":
+            decleration, docs, body = result
+            ret, name, args, gil = decleration
+            stub_file += cdef2str(ret, name, args, gil, docs) + '\n'*2
+        
+        elif definitionName == "cclass":
+            decleration, docs, body = result
+            stub_file += class2str(decleration[1], "", docs) + "\n"
+            for i, b in enumerate(body):
+                if len(b) > 0 and b[0][0] in ["def", "cdef"]:
+                    docs = body[i+1]
+                    ret, name, args, gil = b
+                    stub_file += '\n' + cdef2str(ret, name, args, gil, docs, indent=1) + '\n'
+            stub_file += '\n'
+        
+        elif definitionName == "cstruct":
+            decleration, docs, body = result
+            stub_file += class2str(decleration[1], "", docs) + "\n"
+            for b in body:
+                stub_file += "    " + b + '\n'
+            stub_file += "\n"
+            
+        elif definitionName == "cpdef":
+            decleration, docs, body = result
             ret, name, arg, gil = decleration
             stub_file += cpdef2str(ret, name, arg, gil, docs) + '\n'*2
 
-        elif state == "class":
-            _, name, parent = decleration
+        elif definitionName == "class":
+            decleration, docs, body = result
+            name, parent = decleration
             stub_file += class2str(name, parent, docs) + '\n'
 
             definitions_preset = False
@@ -331,22 +327,23 @@ def parse_tree_to_stub_file(parseTree):
                 definitions_preset = True
                 for b in body:
                     stub_file += '    ' + b + '\n'
-            
+                    
             else:
                 for i, b in enumerate(body):
-                    if len(b) > 0 and b[0] in ["def", "cdef"]:
-                        definitions_preset = True
-                        docs = body[i+1]
-                        state, name, args, ret = b
-                        stub_file += '\n' + def2str(name, args, ret, docs, indent=1) + '\n'
+                    if isinstance(b, ParseResults):
+                        if b.getName() == "def_decleration":
+                            definitions_preset = True
+                            docs = body[i+1]
+                            name, args, ret = b
+                            stub_file += '\n' + def2str(name, args, ret, docs, indent=1) + '\n'
             
             if not definitions_preset:
                 stub_file += "    ..." + '\n'
 
             stub_file += '\n'
             
-        elif state == "dataclass":
-            name = decleration[2]
+        elif definitionName == "dataclass":
+            name, docs, body = result
             stub_file += "@dataclass" + '\n'
             stub_file += f"class {name}:" + '\n'
             stub_file += recursive_body(body, indent=1) + '\n'
--- test.py
+++ test.py
@@ -1,7 +1,36 @@
-argument_test = "test: test[test[test, test], test] = {1: {'a': 'b'}}"
+argument_test = "test: test[test=1] = {1: {'a': 'b'}}"
 parsedResult = argument_definition.parse_string(argument_test)
 
-print(parsedResult)
+for v in parsedResult:
+    print('\t', v)
+    
 print(parsedResult.name)
 print(parsedResult.type)
 print(parsedResult.default)
+
+import sys
+sys.exit()
+
+# def argument_definition():
+#     identifier = Word(alphanums)
+#     argument_forward = Forward()
+#     bracketed_arguments = Group(bracket_suppress(delimited_list(argument_forward)))
+#     argument = Group(OneOrMore(identifier+Optional(DOT)) + Optional(bracketed_arguments))
+#     argument_forward << bracketed_arguments
+#     return argument
+
+
+# python type hint and arguments
+empty_default_return = ParseResults()
+empty_default_return.extend(["", ""])
+
+default_argument = Suppress("=") + expression
+argument = Group(VARIABLE + Optional(Suppress(":") + Group(TYPE + OPTIONALBRACKET), default=["", ""]) + Optional(default_argument, default="")) # VARIABLE:TYPE=DEFAULT
+arguments = delimitedList(argument)
+return_definition = Group(Suppress("->") + TYPE + OPTIONALBRACKET)
+
+# cython type hint and arguments
+cython_type_hint_list = Group(bracket_suppress(delimitedList(Literal(":") | Group(VARIABLE+Optional(default_argument, default="")))))
+cython_type_hint  = (TYPE + Optional(cython_type_hint_list, default=""))
+cython_argument = Group(cython_type_hint + VARIABLE + Optional(default_argument, default=""))
+cython_arguments = delimitedList(cython_argument)
