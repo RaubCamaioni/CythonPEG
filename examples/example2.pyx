4c013ab8e598444e7717fc1d33d696aa2412a039
Raub Camaioni <raub.j.camioni.civ@army.mil>
2023-08-04 18:48:48 -0400

    fixed enum as object

--- examples/example2.pyx
+++ examples/example2.pyx
@@ -1,16 +1,15 @@
-def python_function1(a : Dict[Dict[int, int], int], b = {1: [1, 2, 3]}) -> int:
+def python_function1(a: Dict[Dict[int, int], int], b = {1: [1, 2, 3]}) -> int:
     """complex type return and assignment parsing"""
     return 1
-
-
-undefined parsing data
-Parsing does not know how to parse this
 
 cdef float64 function2(int a, int b):
     """cdef returns and typing"""
     return
 
-cpdef double function3(int a, List[int] b = [1, 2, 3, 4]):
+def test2(interp_flag: int = myenum.SIMPLE_ENUM):
+    return
+
+cpdef double function3(str a = cv2.METHOD, List[int] b = [1, 2, 3, 4]):
     """cpdef return, typing, and assignment """
     return
 
@@ -18,11 +17,6 @@ cdef struct Struct2D:
     """simple struct with cython types"""
     float64** data
     size_t[2] shape
-
-# bad syntax parser will not parse
-cdef struct int Struct5D:
-    size_t a
-    float32* data
 
 @dataclass
 class DataStructure:
